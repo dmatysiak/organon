e553535debf072311974d1eb216f1a632c2bfa7c
Daniel A. Matysiak <dmatysiak@users.noreply.github.com>
2026-05-21 10:21:48 -0400

    extract hardcoded examples

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -3,6 +3,16 @@ declare module "monaco-editor/esm/vs/editor/editor.worker?worker" {
   export default WorkerFactory;
 }
 
+declare module "*.syl?raw" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.tfl?raw" {
+  const content: string;
+  export default content;
+}
+
 declare module "*.json" {
   const value: unknown;
   export default value;
